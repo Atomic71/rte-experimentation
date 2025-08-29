41d64828696b38302bc2f70b28f9255354f6bd29
Atomic71 <tomic@dvica.golf>
2025-08-29 09:31:08 +0200

    chore: remove lexical & slate component code

--- src/editors/slate/index.ts
+++
@@ -1,5 +0,0 @@
-export { SlateEditor } from './SlateEditor';
-export * from './types';
-export * from './utils/serialization';
-export * from './plugins/formatting';
-export * from './plugins/blocks';
