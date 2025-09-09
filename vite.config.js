7e3f61abfd373ffa4ecca5ab876650f761f1f90b
Atomic71 <tomic@dvica.golf>
2025-09-09 16:10:38 +0200

    chore: improve structure

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import path from 'path';
+export default defineConfig({
+  plugins: [react(), viteSingleFile()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: './', // critical for file:// loading in WebView
+  build: {
+    target: 'es2018',
+    outDir: 'dist',
+    assetsInlineLimit: 100000000,
+    cssCodeSplit: false,
+    sourcemap: false,
+  },
+  server: {
+    host: true, // so device can hit your LAN IP
+    port: 5173,
+  },
+});
